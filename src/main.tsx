f420ef774a45caec7e35245fe395be2dddc02250
vahidabsk <vahid.absk@gmail.com>
2026-06-29 00:16:07 -0700

    Support portable file-based build

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { registerServiceWorker } from "./register-service-worker";
 
 registerServiceWorker();
 
+const Router = window.location.protocol === "file:" ? HashRouter : BrowserRouter;
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
